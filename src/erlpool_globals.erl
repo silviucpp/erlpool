b878dc20dbc8886edf6be5c11581c17244acf486
Silviu Caragea <silviu.caragea@woowteam.com>
2017-03-22 12:26:50 +0200

    Fix code reload at runtime

--- src/erlpool_globals.erl
+++ src/erlpool_globals.erl
@@ -1,7 +1,14 @@
 -module(erlpool_globals).
 -author("silviu.caragea").
 
-%stub module. dynamically compiled.
-%created here to hide warnings reported by static analyzers
+%% Module stub.
+%% Will be replaced by the module with the same name at runtime.
+%% The only purpose for the module is to suppress warnings from code analyzers,
+%% as dynamically compiled module is not available during the build.
 
 -export([]).
+
+-on_load(do_not_load/0).
+
+do_not_load() ->
+    do_not_load.
